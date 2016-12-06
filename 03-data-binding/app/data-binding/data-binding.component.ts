8bf49a7cfb7f12cb3f89270ce542bc106a122378
Filipe Amorim da Silva <filipe.amorim.silva@gmail.com>
2016-12-06 13:50:57 -0200

    Aula 08: Instaling Snippets

---
+++ 03-data-binding/app/data-binding/data-binding.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'exemplo-data-biding',
+    templateUrl: 'data-binding.component.html'
+})
+export class DataBindingComponent{
+    constructor() { }
+
+   
+}
